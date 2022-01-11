87d74b3a2b68d4ec56f44d7c2defa6add8dc4aeb
Anna <missjellx@gmail.com>
2022-01-11 17:58:29 +0300

    feat: add number tasks solutions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -201,7 +201,7 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
   let prime = true;
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= n; i += 1) {
     if (n % i === 0 && i !== n) prime = false;
   }
   return prime;
